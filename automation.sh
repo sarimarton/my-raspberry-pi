3f0b6afcc4622315463e5d92fcdc92aa8217c143
sarimarton <sarimarton@gmail.com>
2021-10-12 17:40:51 +0200

    Fix script

--- automation.sh
+++ automation.sh
@@ -1,5 +1,3 @@
-sleep 10s
-
 # try with 2 coordinates, because the 1st one may be occupied by Focusrite
 xdotool mousemove 1720 20 click 1
 sleep 1s
@@ -14,7 +12,7 @@ xdotool click 1
 sleep 5s
 
 aid=$(for id in $(xdotool search --class 'AcrossCenter'); do
-  if [ "Across Center" == "$(xdotool getwindowname $id)" ]; then
+  if [ "Across Center" = "$(xdotool getwindowname $id)" ]; then
     echo $id
   fi
 done)
